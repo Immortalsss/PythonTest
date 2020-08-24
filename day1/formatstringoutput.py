ddc4109c2321efb49ca3d1761d7903c7f74a73e3
sunhaibo <1393240100@qq.com>
2020-08-24 10:32:28 +0800

    if语句学习

--- day1/formatstringoutput.py
+++ day1/formatstringoutput.py
@@ -1,7 +1,7 @@
 name = 'Tom'
 age = 18
 
-print('我的名字是%s，今年%d岁了' % (name,age))
+print('我的名字是%s，今年%d岁了' % (name, age))
 
 # f'{表达式}'
 print(f'我的名字是{name}，今年{age}岁了')
---
+++ day2/finger_guessing_game.py
@@ -0,0 +1,14 @@
+import random
+
+player = int(input('请出拳：0--石头，1--剪刀，2--布：'))
+
+computer = random.randint(0, 2)
+
+print(f'电脑出拳是：{computer}')
+
+if ((player == 0) and (computer == 1)) or ((player == 1) and (computer == 2)) or ((player == 2) and (computer == 0)):
+    print('玩家获胜')
+elif ((player == 0) and (computer == 0)) or ((player == 1) and (computer == 1)) or ((player == 2) and (computer == 2)):
+    print('平局')
+else:
+    print('电脑获胜')
